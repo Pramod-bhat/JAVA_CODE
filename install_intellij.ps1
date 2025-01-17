ef4b1d96e56e1a7da246ec74f0046ad7b96ab867
Pramod Bhat <pramodbhat94@gmail.com>
2025-01-17 18:30:41 +0530

    Create install_intellij.ps1

---
+++ install_intellij.ps1
@@ -0,0 +1,23 @@
+# Save this script as `install_intellij.ps1`
+
+# Define IntelliJ IDEA version and download URL
+$version = "2023.2"  # Replace with the desired version
+$intellij_url = "https://download.jetbrains.com/idea/ideaIC-$version.exe"
+
+# Download IntelliJ IDEA installer
+Write-Host "Downloading IntelliJ IDEA..."
+$output_path = "$env:TEMP\ideaIC-$version.exe"
+Invoke-WebRequest -Uri $intellij_url -OutFile $output_path
+
+# Silent installation parameters
+$arguments = "/S"
+
+# Install IntelliJ IDEA
+Write-Host "Installing IntelliJ IDEA..."
+Start-Process -FilePath $output_path -ArgumentList $arguments -Wait
+
+# Cleanup
+Write-Host "Cleaning up..."
+Remove-Item -Path $output_path
+
+Write-Host "IntelliJ IDEA installation complete."
